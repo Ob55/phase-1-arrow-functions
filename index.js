5539d71c85c4b4a10a25316189dfbba90c9a072e
Ob55 <brian55mwangi@gmail.com>
2023-12-16 17:40:44 +0300

    done assigment

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function(){
+     let sum = 2000 / 100 
+     return sum 
+}
+ const square = () =>{
+     let square = 2*2
+     return square
+ }
+  const add = () => {
+     let a = 3
+     let b = 4
+     let add = a + b
+     return add         
+  }
+  
